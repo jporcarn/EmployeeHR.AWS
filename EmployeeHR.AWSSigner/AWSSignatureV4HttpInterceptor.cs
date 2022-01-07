621335e9ba299afd3f983aab794d805b0141ace3
jporcarohpen <jporcar@ohpen.com>
2022-01-07 21:45:16 +0100

    AWS Request Signature version4 done by the AWS SDK for .NET and using the functionality from the SDK to actually perform the signature.

---
+++ EmployeeHR.AWSSigner/AWSSignatureV4HttpInterceptor.cs
@@ -0,0 +1,76 @@
+﻿using Amazon.ApiGatewayManagementApi;
+using Amazon.Runtime.Internal;
+using Amazon.Runtime.Internal.Auth;
+using Amazon.Runtime.Internal.Util;
+using Amazon.Util;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+// aws-signer-v4-using-sdk-net
+
+namespace EmployeeHR.AWSSigner
+{
+    /// <summary>
+    /// AWS Signature Version 4 Http Interceptor
+    /// </summary>
+    /// <see cref="https://docs.aws.amazon.com/AmazonS3/latest/API/sig-v4-header-based-auth.html"/>
+    public class AWSSignatureV4HttpInterceptor : DelegatingHandler
+    {
+        private readonly string _accessKeyId;
+        private readonly string _secretAccessKey;
+        private readonly string _region;
+        private readonly string? _sessionToken;
+        private readonly string _service = "execute-api";
+
+        public AWSSignatureV4HttpInterceptor(string accessKeyId, string secretAccessKey, string region, string? sessionToken = null) : base(new HttpClientHandler())
+        {
+            this._accessKeyId = accessKeyId;
+            this._secretAccessKey = secretAccessKey;
+            this._region = region;
+            this._sessionToken = sessionToken;
+        }
+
+        async protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+
+            if (_sessionToken != null)
+            {
+                request.Headers.Add(HeaderKeys.XAmzSecurityTokenHeader, _sessionToken);
+            }
+
+            Sign(request, this._accessKeyId, this._secretAccessKey, this._region, this._service);
+
+            HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
+
+            return response;
+        }
+
+
+        private void Sign(HttpRequestMessage request, string awsAccessKeyId, string awsSecretAccessKey, string region, string service)
+        {
+
+            bool signPayload = true;
+
+            AWS4Signer signer = new AWS4Signer(signPayload);
+
+            var metrics = new RequestMetrics();
+
+            var clientConfig = new AmazonApiGatewayManagementApiConfig();
+
+            IRequest executeApiRequest = new AmazonApiGatewayManagementRequest(request);
+
+            AWS4SigningResult result = signer.SignRequest(executeApiRequest, clientConfig, metrics, awsAccessKeyId, awsSecretAccessKey);
+
+            executeApiRequest.Headers.ToList().ForEach(h =>
+            {
+                request.Headers.TryAddWithoutValidation(h.Key, h.Value);
+            });
+
+            request.Headers.TryAddWithoutValidation("Authorization", result.ForAuthorizationHeader);
+        }
+
+    }
+}
---
+++ EmployeeHR.AWSTests/Controllers/ValuesControllerTests.cs
@@ -0,0 +1,74 @@
+﻿using EmployeeHR.AWSSigner;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EmployeeHR.AWS.Controllers.Tests
+{
+    public class ValuesControllerTests
+    {
+        private readonly string _region = "eu-west-1";
+        private readonly string _accessKeyId = "";
+        private readonly string _secretAccessKey = "";
+        private readonly Uri _baseAddress;
+
+        public ValuesControllerTests()
+        {
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            _accessKeyId = config.GetValue<string>("AWS:access_key_id");
+            _secretAccessKey = config["AWS:secret_access_key"];
+            _region = config["AWS:region"];
+            _baseAddress = new Uri(config["Request:baseAddress"]);
+
+        }
+
+
+        [Theory()]
+        [InlineData("Stage")]
+        [InlineData("Prod")]
+        public async Task GetValuesTest(string stage)
+        {
+            var client = new HttpClient(new AWSSignatureV4HttpInterceptor(_accessKeyId, _secretAccessKey, _region))
+            {
+                BaseAddress = this._baseAddress
+            };
+
+            HttpResponseMessage? response = null;
+
+            try
+            {
+                var requestUri = $"{stage}/api/values";
+                response = await client.GetAsync(requestUri);
+
+                response.EnsureSuccessStatusCode();
+
+                Assert.NotNull(response);
+            }
+            catch (HttpRequestException ex)
+            {
+
+                string message = ex.Message;
+                if (response != null)
+                {
+                    message = await response.Content.ReadAsStringAsync();
+                }
+
+                Assert.False(true, message);
+            }
+            catch (Exception ex)
+            {
+                Assert.False(true, ex.Message);
+                // throw;
+            }
+        }
+
+    }
+}
